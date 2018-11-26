5fb13a41e4386ccee5bbe9cf818411ecfa6a6e4f
imooc <pengsel@qq.com>
2018-11-26 12:28:46 +0800

    增加保存为csv文件功能

--- Li_video/Li_video.py
+++ Li_video/Li_video.py
@@ -1,29 +1,36 @@
 import re
 from lxml import etree
 import requests
-base_url = "http://www.pearvideo.com/"
-url=base_url+"category_2"
 
 def download_video(url,name):
     response= requests.get(url)
+    # 去除名字里的引号
+    name=re.sub("[‘’“”\"']","",name)
+    print(name)
     with open(name+'.mp4', "ab+") as f:
         f.write(response.content)
 def get_video(url):
     response= requests.get(url)
     html = response.text
-    link = re.findall('srcUrl=".*?"',html)
+    link = re.findall('srcUrl="(.*?)"',html)
     print(link)
     name=re.findall('<title>(.*?)</title>',html)
     print(name)
-    download_video(url,name[0])
+    # 去除直播链接
+    list=re.findall("直播：",name)
+    if len(list)==0:
+        download_video(link[0],name[0])
 def LiIndex(url):
     response=requests.get(url)
     html=response.text
     html_xpath = etree.HTML(html)
+    # Xpath提取url
     urls=html_xpath.xpath('//li/div/a/@href')
     print(urls)
     for liurl in urls:
         get_video(base_url+liurl)
 
-
+base_url = "http://www.pearvideo.com/"
+#梨视频中的“国际”分类
+url=base_url+"category_2"
 LiIndex(url)
---
+++ Li_video/xpath.md
@@ -0,0 +1,3 @@
+## [XPath](http://www.runoob.com/xpath/xpath-syntax.html) 
+* 使用路径表达式在 XML 文档中选取节点。
+ 
